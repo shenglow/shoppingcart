bda27662501df1417f155dc04f99e2a927f5a0c1
boonlow <boon_100795@castech.com.tw>
2019-07-10 15:08:06 +0800

    [190710][ShoppingCart - P]add show new arrival
    show new arrival product in index page

--- app/Http/Controllers/Front/WebsiteController.php
+++ app/Http/Controllers/Front/WebsiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
 
 class WebsiteController extends Controller
 {
@@ -15,6 +16,8 @@ class WebsiteController extends Controller
     {
         $user = Auth::user();
 
-        return view('front.index', ['user' => $user]);
+        $products = Product::where('is_enable', true)->orderBy('created_at', 'desc')->take(10)->get();
+
+        return view('front.index', ['user' => $user, 'products' => $products,]);
     }
 }
