efa39c136fb3cb4cceacd605134f8084a2842085
boonlow <boon_100795@castech.com.tw>
2019-07-26 18:13:18 +0800

    [190726][ShoppingCart - D]fix logout modal content

--- resources/views/back/layouts/main.blade.php
+++ resources/views/back/layouts/main.blade.php
@@ -60,15 +60,15 @@
         <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <h5 class="modal-title" id="exampleModalLabel">確定登出?</h5>
             <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">×</span>
             </button>
             </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-body">如果您確定要登出,請選按底下的"登出".</div>
             <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="{{ route('admin.logout') }}">Logout</a>
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">取消</button>
+            <a class="btn btn-primary" href="{{ route('admin.logout') }}">登出</a>
             </div>
         </div>
         </div>
